cf057d68aef40567957d5be930628cc4170f4beb
N T R Phani Kumar <ntrphanikumar@gmail.com>
2023-05-20 12:08:24 +0530

    Handled all known device types and names

---
+++ app/pages/api/agent/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const uap = require('./parser');
+export default (req, res) => {
+    let rawdata = fs.readFileSync('useragents.json');
+    var agents = JSON.parse(rawdata).aggregations.distinct_user_agents.buckets.map(e => unescape(e.key)).map(agent => uap.deviceTypeAndName(agent))
+    var processedDeviceNames = ["Samsung Phone", "Amazon Fire TV", "Apple TV", "Web0S TV", "Mi Box", "Roku", "Chromecast", "C00 TV", "UnionTV","Apple iPad","Sony Bravia", "Mi TV","D00 TV","Apple iPhone","Skyworth TV","OnePlus TV","Haier TV","BeyondTV","HiSmartTV A4","LG Web0S SmartTV","TCL HbbTV","Kodak TV","Sony Phone","K Phone","Motorola Phone","AI PONT TV","Kogan TV", "Lenovo Phone","Cloud TV","OnePlus Phone", "OPPO Phone","Vivo Phone","PANASONIC TV","Huawei Phone","Philips TV","Scope Box","Oppo Phone","Amazon Fire Tablet", "Linux TV","Xiaomi Phone","Nokia Phone","Amazon Kindle","ASUS Phone","Realme Phone","LG NetCast","Google Phone","LG Phone","Infinix Phone", "Samsung SMART-TV","SERAPHIC HbbTV","Itel Phone","Amazon Android Tablet","Panasonic Phone","Sharp Phone","Tecno Phone","LYF Phone","POLYTRON Phone", "Windows Computer", "Linux Computer","Chrome OS Computer","Apple Mac", "Other", "Apple AirPlay","Android TV","Android Phone"]
+
+
+    var devicesToRevisit = []
+    agents = agents.filter(agent => !processedDeviceNames.includes(agent.deviceName) && !devicesToRevisit.includes(agent.deviceName))
+    console.log(agents)
+    res.status(200).json(agents);
+};
---
+++ app/pages/api/agent/parser.js
@@ -0,0 +1,184 @@
+const fs = require('fs');
+const parser = require('ua-parser-js');
+const uap = require('node-uap');
+const merge = require('deepmerge')
+const Set = require("collections/set");
+
+function deviceTypeAndName(userAgent) {
+    if (userAgent == '-') return {
+        "deviceType": "Unknown",
+        "deviceName": "Unknown"
+    }
+    let uapData = uap.parse(unescape(userAgent))
+    let uaParserData = parser(unescape(userAgent))
+    let deviceName = 'Unknown', deviceType = 'Unknown'
+    console.log(userAgent)
+    console.log("UAPData", uapData)
+    console.log("UAParserData", uaParserData)
+    if (uaParserData.device.type == "smarttv") {
+        deviceType = "Smart TV"
+        if (uaParserData.device.vendor == "Apple") {
+            deviceName = "Apple TV"
+        } else if (uaParserData.device.model && uaParserData.device.model.startsWith("Chromecast")) {
+            deviceName = "Chromecast"
+        } else if (uaParserData.device.vendor == "Roku") {
+            deviceName = "Roku"
+        } else if (uaParserData.device.vendor == 'Amazon' && uapData.device.model.startsWith("AFT")) {
+            deviceName = "Amazon Fire TV"
+        } else if (uaParserData.device.vendor == 'Xiaomi' && uapData.device.model.startsWith("MiTV")) {
+            deviceName = "Mi TV"
+        } else if (uapData.device.brand == "Haier") {
+            deviceType = "Smart TV"
+            deviceName = "Haier TV"
+        } else if (uapData.device.brand == "Philips") {
+            deviceType = "Smart TV"
+            deviceName = "Philips TV"
+        } else if (uaParserData.device.vendor == 'Sony') {
+            if (uaParserData.device.model.toUpperCase().startsWith("BRAVIA")) {
+                deviceName = "Sony Bravia"
+            } else {
+                deviceName = "Sony " + uaParserData.device.model
+            }
+        } else if (uapData.device.brand && !uapData.device.brand.startsWith("Generic") && uapData.device.family) {
+            if (uapData.device.family.indexOf(uapData.device.brand) == -1) {
+                deviceName = uapData.device.brand + " " + uapData.device.family
+            } else {
+                deviceName = uapData.device.family
+            }
+        } else {
+            deviceName = uapData.os.family + " TV"
+        }
+    } else if (uaParserData.device.type == "mobile") {
+        deviceType = "Mobile Phone"
+        if (uaParserData.device.vendor == "Apple") {
+            deviceName = "Apple " + uaParserData.device.model
+        } else if (uapData.device.model == "PANASONIC TV") {
+            deviceType = "Smart TV"
+            deviceName = "PANASONIC TV"
+        } else if (uaParserData.device.vendor) {
+            deviceName = uaParserData.device.vendor + " Phone"
+        } else if (uapData.device.brand == "Generic_Android" || uapData.device.brand == "Generic") {
+            if (uapData.device.model == "M2006C3MI") {
+                deviceName = "Xiaomi Phone"
+            } else if (uapData.device.model == "K") {
+                deviceName = "K Phone"
+            } else {
+                deviceName = "Android Phone"
+            }
+        } else {
+            deviceName = (uapData.device.brand || uaParserData.os.name) + " Phone"
+        }
+    } else if (uaParserData.device.type == "tablet") {
+        deviceType = "Tablet"
+        if (uaParserData.device.vendor == "Apple") {
+            deviceName = "Apple iPad"
+        } else if (uaParserData.device.vendor == "Amazon") {
+            if (uaParserData.device.model == "KFMAWI") {
+                deviceName = "Amazon Fire Tablet"
+            } else if (uaParserData.device.model == "KFKAWI" || uaParserData.device.model == "KFAPWI") {
+                deviceName = "Amazon Kindle"
+            } else if (uapData.device.brand == "Generic_Android") {
+                deviceName = "Amazon Android Tablet"
+            } else {
+                deviceName = "Amazon " + uaParserData.os.name + " Tablet"
+            }
+        } else if (uaParserData.device.vendor == "Samsung") {
+            deviceName = uaParserData.device.vendor + " Phone"
+        } else {
+            deviceName = uaParserData.os.name + " Phone"
+        }
+    } else if (uaParserData.device.type == "wearable") {
+        deviceType = "Wearable"
+        deviceName = uaParserData.device.vendor
+    } else if (!uaParserData.device.type && uaParserData.cpu.architecture) {
+        deviceType = "Computer"
+        deviceName = (uapData.os.family == "Ubuntu" ? "Linux" : uapData.os.family) + " Computer"
+    } else {
+        if (!uapData.device.model) {
+            if (userAgent.indexOf('TV Store') > -1) {
+                deviceType = "Smart TV"
+                deviceName = uapData.os.family + " TV"
+            } else if(uapData.device.family == "Other" && uapData.os.family != "Other") {
+                deviceType = "Computer"
+                deviceName = uapData.os.family + " Computer"
+            } else if (uapData.os.family == "Other") {
+                if(userAgent.indexOf("AirPlay")>-1) {
+                    deviceType = "Smart TV"
+                    deviceName = "Apple AirPlay"
+                } else {
+                    deviceType = "Other"
+                    deviceName = uapData.device.family
+                }
+            } else {
+                deviceName = unescape(uapData.ua.family)
+            }
+        } else if (uapData.device.model == "iOS-Device") {
+            deviceType = "Mobile Phone"
+            deviceName = "Apple iPhone"
+        } else if (uapData.device.model == "Mac") {
+            deviceType = "Computer"
+            deviceName = "Apple Mac"
+        } else if (uapData.device.model.startsWith("AFT")) {
+            deviceType = "Smart TV"
+            deviceName = "Amazon Fire TV"
+        } else if (uapData.device.model.startsWith("MIBOX")) {
+            deviceType = "Smart TV"
+            deviceName = "Mi Box"
+        } else if (uapData.device.model.startsWith("MiTV")) {
+            deviceType = "Smart TV"
+            deviceName = "Mi TV"
+        } else if (uapData.device.model.startsWith("Chromecast")) {
+            deviceType = "Smart TV"
+            deviceName = "Chromecast"
+        } else if (uapData.device.model.startsWith("SWTV")) {
+            deviceType = "Smart TV"
+            deviceName = "Skyworth TV"
+        } else if (uapData.device.model.startsWith("Y Series") || uapData.device.model == "Oneplus_Dosa_IN") {
+            deviceType = "Smart TV"
+            deviceName = "OnePlus TV"
+        } else if (uapData.device.model == "haierATVnippori") {
+            deviceType = "Smart TV"
+            deviceName = "Haier TV"
+        } else if (uapData.device.model == "PATH_7XPRO") {
+            deviceType = "Smart TV"
+            deviceName = "Kodak TV"
+        } else if (uapData.device.model == "UnionTV") {
+            deviceType = "Smart TV"
+            deviceName = "UnionTV"
+        } else if (uapData.device.model == "TPM191E") {
+            deviceType = "Smart TV"
+            deviceName = "Philips TV"
+        } else if (uapData.device.model == "Smartphone") {
+            deviceType = "Mobile Phone"
+            deviceName = uaParserData.os.name + " Phone"
+        } else if (userAgent.indexOf("AndroidTV") > -1) {
+            deviceType = "Smart TV"
+            if (uapData.device.model == "SMART_TV" || uaParserData.device.vendor == "Generic") {
+                deviceName = uaParserData.os.name + " TV"
+            } else {
+                deviceName = uapData.device.model + (uapData.device.model.toUpperCase().indexOf("TV") > -1 ? "" : " TV")
+            }
+        } else if (userAgent.indexOf("AndroidMobile") > -1) {
+            deviceType = "Mobile Phone"
+            deviceName = uaParserData.os.name + " Phone"
+        } else if (userAgent.indexOf("CloudWalker") > -1) {
+            deviceType = "Smart TV"
+            deviceName = "Cloud TV"
+        } else if (userAgent.indexOf("ScopeBox") > -1) {
+            deviceType = "Smart TV"
+            deviceName = "Scope Box"
+        } else if (uapData.device.family && uapData.device.family.startsWith("CloudTV")) {
+            deviceType = "Smart TV"
+            deviceName = "Cloud TV"
+        } else if ((uaParserData.browser.name || unescape(uapData.ua.family)) != "Other") {
+            deviceName = uaParserData.browser.name || unescape(uapData.ua.family)
+        }
+    }
+    return {
+        "agent": userAgent,
+        "deviceType": deviceType,
+        "deviceName": deviceName
+    }
+}
+
+module.exports = { deviceTypeAndName };
